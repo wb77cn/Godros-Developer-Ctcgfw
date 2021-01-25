665b5e962f8f08ced607cfc19a50de68c096cc82
godros <61609258+godros@users.noreply.github.com>
2021-01-25 12:09:47 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-echo '
-CONFIG_CRYPTO_CHACHA20_X86_64=y
-CONFIG_CRYPTO_POLY1305_X86_64=y
-' >> ./target/linux/x86/64/config-5.4
+#!/bin/bash
+# 修改主机名字
+#sed -i 's/OpenWrt/GodROS/g' package/base-files/files/bin/config_generate
+#添加img编译时间前缀。
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-godros-1907-Stable-X64-/g' include/image.mk
+# 修改内核版本
+#sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
