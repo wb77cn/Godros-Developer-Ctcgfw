e31ddef46b4e80b09a0af6fbe672ad2e7c0663ca
godros <61609258+godros@users.noreply.github.com>
2021-01-27 09:17:28 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -3,6 +3,6 @@
 # 修改主机名字
 #sed -i 's/OpenWrt/GodROS/g' package/base-files/files/bin/config_generate
 #添加img编译时间前缀。
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-GodROS-D-/g' include/image.mk
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d)-GodROS-Developer-/g' include/image.mk
 # 修改内核版本
 #sed -i 's/KERNEL_PATCHVER:=4.14/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
